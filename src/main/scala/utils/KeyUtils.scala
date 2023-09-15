5b34f79d247d0db370f6f05d2f9e4dd16068e182
Muqiu Han <muqiu-han@outlook.com>
2023-09-15 19:46:29 +0800

    Refactor: Use boundary.break instead of local return

--- src/main/scala/utils/KeyUtils.scala
+++ src/main/scala/utils/KeyUtils.scala
@@ -12,38 +12,42 @@ object KeyUtils {
             val part2: Array[String] = key.split("\\.")
             val len2: Int = part2.length
             val len1: Int = part1.length
-            if (key.contains("#")) {
-                var i: Int = -1
-                if (part2.head == "#") {
-                    i = 1
-                    while (
-                      i <= math.min(len1, len2) && part2(len2 - i) == part1(
-                        len1 - i
-                      )
-                    ) {
-                        i += 1
+
+            boundary:
+                if (key.contains("#")) {
+                    var i: Int = -1
+                    if (part2.head == "#") {
+                        i = 1
+                        while (
+                          i <= math.min(len1, len2) && part2(len2 - i) == part1(
+                            len1 - i
+                          )
+                        ) {
+                            i += 1
+                        }
+                        if (part2(len2 - i) == "#") { break(true) }
+                    } else if (part2.last == "#") {
+                        i = 0
+                        while (
+                          i < math.min(len1, len2) && part2(i) == part1(i)
+                        ) {
+                            i += 1
+                        }
+                        if (part2(i) == "#") { break(true) }
                     }
-                    if (part2(len2 - i) == "#") { return true }
-                } else if (part2.last == "#") {
-                    i = 0
-                    while (i < math.min(len1, len2) && part2(i) == part1(i)) {
-                        i += 1
+                } else {
+                    var flag: Boolean = true
+                    if (len1 == len2) {
+                        for (
+                          i <- 0 until len1
+                          if !(part2(i) == "*" || part1(i) == part2(i))
+                        ) {
+                            flag = false
+                        }
+                        if (flag) { break(true) }
                     }
-                    if (part2(i) == "#") { return true }
                 }
-            } else {
-                var flag: Boolean = true
-                if (len1 == len2) {
-                    for (
-                      i <- 0 until len1
-                      if !(part2(i) == "*" || part1(i) == part2(i))
-                    ) {
-                        flag = false
-                    }
-                    if (flag) { return true }
-                }
-            }
-            false
+                false
         )
     }
 }
