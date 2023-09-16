0c7805f2fe86ced6d5dbfcb8090d245ee72ff0cd
Muqiu Han <muqiu-han@outlook.com>
2023-09-16 15:18:12 +0800

    Update: Change the banner output logic

--- src/main/scala/utils/Banner.scala
+++ src/main/scala/utils/Banner.scala
@@ -1,39 +1,45 @@
 package utils
 
 import com.typesafe.scalalogging.Logger
+import scala.Console._
 import scala.io.Source
 
 object Banner {
     private val log = Logger(getClass)
 
-    private val banner = """"""
+    private val banner = """
+          :::    :::        :::        ::::    :::      :::   :::      :::::::: 
+         :+:    :+:      :+: :+:      :+:+:   :+:     :+:+: :+:+:    :+:    :+: 
+        +:+    +:+     +:+   +:+     :+:+:+  +:+    +:+ +:+:+ +:+   +:+    +:+  
+       +#++:++#++    +#++:++#++:    +#+ +:+ +#+    +#+  +:+  +#+   +#+    +:+   
+      +#+    +#+    +#+     +#+    +#+  +#+#+#    +#+       +#+   +#+    +#+    
+     #+#    #+#    #+#     #+#    #+#   #+#+#    #+#       #+#   #+#    #+#     
+    ###    ###    ###     ###    ###    ####    ###       ###    ###########
+
+        o- Github : https://github.com/muqiuhan/HanMQ
+        o- Version: 0.0.1
+        o- Copyright (c) 2022 Muqiu Han
+    """
 
     def load(): Unit = {
+        println()
+        val banner = Source
+            .fromFile(
+              getClass.getClassLoader().getResource("banner").getPath()
+            )
+            .getLines
+
         System.getProperty("os.name") match {
-            case "Linux" | "Macos" => printLinux
-            case "Windows"         => printWindows
+            case "Linux" | "Macos" =>
+                println(
+                  banner.mkString("\u001b[38;5;10m", "\n", "\u001b[0m")
+                )
+
+            case "Windows" =>
+                println(
+                  banner.mkString("\n")
+                )
         }
-    }
 
-    def printLinux: Unit = {
-        println(
-          Source
-              .fromFile(
-                getClass.getClassLoader().getResource("banner-linux").getPath()
-              )
-              .getLines
-              .mkString("\u001b[38;5;10m", "\n", "\u001b[0m")
-        )
-    }
-
-    def printWindows: Unit = {
-        println(
-          Source
-              .fromFile(
-                getClass.getClassLoader().getResource("banner-linux").getPath()
-              )
-              .getLines
-              .mkString("\n")
-        )
     }
 }
