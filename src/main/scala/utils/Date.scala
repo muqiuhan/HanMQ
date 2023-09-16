7ebfc7bc9e5688c5c348914f72c3335a0a607f05
Muqiu Han <muqiu-han@outlook.com>
2023-09-16 15:18:34 +0800

    Add: utils.Date

---
+++ src/main/scala/utils/Date.scala
@@ -0,0 +1,10 @@
+package utils
+
+import java.text.SimpleDateFormat
+import java.util
+
+object Date {
+    def getLocalTime(): String = {
+        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new util.Date())
+    }
+}
